a25b3ba6924591d8895b34a13b2e24b045ce0e62
CardoTisler <cardotisler@proton.me>
2023-04-14 00:31:01 +0300

    Vitest unit tests setup

---
+++ src/lib/components/AccessibilityModal.spec.ts
@@ -0,0 +1,12 @@
+import AccessibilityModal from "./AccessibilityModal.svelte";
+import { render, screen } from '@testing-library/svelte';
+import { describe, expect, it } from 'vitest';
+
+describe('AccessibilityModal.svelte', () => {
+    it('should render component', () => {
+        const toggleMenu = () => {return};
+        const { container } = render(AccessibilityModal, { toggleMenu });
+
+        expect(container).toMatchSnapshot();
+    });
+})
